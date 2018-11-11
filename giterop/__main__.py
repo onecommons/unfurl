b8abbe84a67a570647f6c95bb36d1b17926b6174
adam souzis <adam@souzis.com>
2018-11-10 16:27:49 -0800

    add cli options for logging

--- giterop/__main__.py
+++ giterop/__main__.py
@@ -7,17 +7,45 @@ For each configuration checks if it should be run, records the result
 from .manifest import runJob
 from . import __version__
 import click
-import sys
-import traceback
+import logging
+logger = logging.getLogger('gitup')
+
+def initLogging(quiet, logfile, verbose):
+  logging.captureWarnings(True)
+  logger.setLevel(logging.DEBUG)
+
+  if logfile:
+    ch = logging.FileHandler(logfile)
+    formatter = logging.Formatter(
+        '[%(asctime)s] %(message)s')
+    ch.setFormatter(formatter)
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+
+  ch = logging.StreamHandler()
+  formatter = logging.Formatter('%(message)s')
+  ch.setFormatter(formatter)
+
+  if quiet:
+    ch.setLevel(logging.CRITICAL)
+  else:
+    # TRACE (5)
+    levels = [logging.INFO, logging.DEBUG, 5, 5]
+    ch.setLevel(levels[verbose])
+  logger.addHandler(ch)
 
 @click.group()
 @click.pass_context
 @click.option('-v', '--verbose', count=True)
-def cli(ctx, verbose=0):
+@click.option('-q', '--quiet', default=False, help='Only output errors to the stdout')
+@click.option('--logfile', default=None,
+                              help='Log file for messages during quiet operation')
+def cli(ctx, verbose=0, quiet=False, logfile=None):
   # ensure that ctx.obj exists and is a dict (in case `cli()` is called
   # by means other than the `if` block below
   ctx.ensure_object(dict)
   ctx.obj['verbose'] = verbose
+  initLogging(quiet, logfile, verbose)
 
 @cli.command()
 @click.pass_context
@@ -25,9 +53,10 @@ def cli(ctx, verbose=0):
 @click.option('--resource', help="name of resource to start with")
 @click.option('--add', default=True, help="run newly added configurations")
 @click.option('--update', default=True, help="run configurations that whose spec has changed but don't require a major version change")
-@click.option('--repair', type=click.Choice(['error', 'degraded', 'none']),
+@click.option('--repair', type=click.Choice(['error', 'degraded', 'notapplied', 'none']),
   default="error", help="re-run configurations that are in an error or degraded state")
 @click.option('--upgrade', default=False, help="run configurations with major version changes or whose spec has changed")
+@click.option('--dryrun', default=False, help='Do not modify anything, just do a dry run.')
 def run(ctx, manifest, **options):
   options.update(ctx.obj)
   try:
