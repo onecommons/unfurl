33b800fd5fc51d165e9f2a779e6f3f3ac7f16b57
adam souzis <adam@souzis.com>
2019-02-13 20:09:05 -0800

    remove implementation of old model

--- giterop/__init__.py
+++ giterop/__init__.py
@@ -1,8 +1,3 @@
-from .util import *
-from .run import *
-
-import sys
-
 """
 Basic operation of GitErOp is to apply the specified configuration to a resource
 and record the results of the application.
@@ -13,15 +8,6 @@ rely on git history for this. In fact a git repository isn't required at all.
 But the intent is for commits in a git repo to correspond to reproducible configuration states of the system.
 The git repo is also used to record or archive exact versions of each configurators applied.
 """
-
-#cmd=update find and run configurations that need to be applied
-#for running and recording adhoc changes:
-#cmd=add resource configuration action params
-def run(manifestPath, opts=None):
-  manifest = Manifest(path=manifestPath)
-  runner = Runner(manifest)
-  kw = opts or {}
-  return runner.run(**kw)
 
 from pkg_resources import get_distribution
 __version__ = get_distribution(__name__).version
