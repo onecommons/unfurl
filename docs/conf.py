c4d3fa69905bec1ce56fe7b671402d81bc000fea
Matthew Souzis <matthew@souzis.com>
2020-01-05 19:42:55 -0800

    add light logo etc

--- docs/conf.py
+++ docs/conf.py
@@ -44,21 +44,22 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme_options = {
-    "show_sidebar": False,
-    "navbar_bg_class": 'faded',
-    "navbar_color_class": 'light'
+    # "show_sidebar": True,
+    # "navbar_bg_class": 'faded',
+    # "navbar_color_class": 'light',
     # "sidebar_fixed": False
 }
 
-# html_theme = "alabaster"
+# default theme:
+# html_theme = "alabaster" 
 
-html_theme = "sphinxbootstrap4theme"
 import sphinxbootstrap4theme
+html_theme = "sphinxbootstrap4theme"
 
 html_theme_path = [sphinxbootstrap4theme.get_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-html_logo = "./unfurl_logo.svg"
+html_static_path = ['_static']
+html_logo = "./unfurl_logo_light.svg"
