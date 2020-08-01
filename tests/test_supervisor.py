cc9a47afa5c2193d9717327e969f1a6607bfa173
adam souzis <adam@souzis.com>
2020-08-01 13:44:22 -0700

    add a configurator for Supervisor

---
+++ tests/test_supervisor.py
@@ -0,0 +1,96 @@
+import unittest
+import os
+import json
+import time
+from six.moves import urllib
+from click.testing import CliRunner
+
+import unfurl.configurators  # python2.7 workaround
+import unfurl.configurators.shell  # python2.7 workaround
+import unfurl.configurators.supervisor  # python2.7 workaround
+from unfurl.yamlmanifest import YamlManifest
+from unfurl.job import Runner, JobOptions
+
+manifest = """\
+apiVersion: unfurl/v1alpha1
+kind: Ensemble
+spec:
+  service_template:
+    imports:
+      - repository: unfurl
+        file: configurators/supervisor-template.yaml
+
+    topology_template:
+      node_templates:
+        # localhost:
+        #   directives:
+        #   - select
+        #   type: tosca.nodes.Compute
+
+        supervisord:
+          type: unfurl.nodes.Supervisor
+          properties:
+            homeDir: . # unix socket paths can't be > 100 characters
+          # requirements:
+          # - host: localhost
+
+        unfurl_run_runner:
+          type: unfurl.nodes.ProcessController.Supervisor
+          properties:
+            name: test
+            program:
+              command: python3 -m http.server -b 127.0.0.1 8012
+              redirect_stderr: true
+              stdout_logfile: '%(here)s/test.log'
+          requirements:
+          - host: supervisord
+"""
+
+
+class SupervisorTest(unittest.TestCase):
+    def test_supervisor(self):
+        cliRunner = CliRunner()
+        with cliRunner.isolated_filesystem():
+            runner = Runner(YamlManifest(manifest))
+
+            job = runner.run(JobOptions(startTime=1))  # deploy
+            assert not job.unexpectedAbort, job.unexpectedAbort.getStackTrace()
+            summary = job.jsonSummary()
+            self.assertEqual(
+                {
+                    "id": "A01110000000",
+                    "status": "ok",
+                    "total": 4,
+                    "ok": 4,
+                    "error": 0,
+                    "unknown": 0,
+                    "skipped": 0,
+                    "changed": 3,
+                },
+                summary["job"],
+            )
+
+            # print(json.dumps(summary, indent=2))
+            time.sleep(0.15)
+            f = urllib.request.urlopen("http://127.0.0.1:8012/")
+            expected = b"Directory listing for /"
+            self.assertIn(expected, f.read())
+
+            runner = Runner(YamlManifest(job.out.getvalue()))
+            job = runner.run(JobOptions(workflow="undeploy", startTime=2))
+            assert not job.unexpectedAbort, job.unexpectedAbort.getStackTrace()
+            summary = job.jsonSummary()
+            # print(json.dumps(summary, indent=2))
+            self.assertEqual(
+                {
+                    "id": "A01120000000",
+                    "status": "ok",
+                    "total": 3,
+                    "ok": 3,
+                    "error": 0,
+                    "unknown": 0,
+                    "skipped": 0,
+                    "changed": 3,
+                },
+                summary["job"],
+            )
---
+++ unfurl/configurators/supervisor.py
@@ -0,0 +1,96 @@
+from __future__ import absolute_import
+import os.path
+
+import six
+from six.moves.configparser import ConfigParser
+
+from supervisor import xmlrpc
+
+try:
+    from xmlrpc.client import ServerProxy, Fault
+except ImportError:
+    from xmlrpclib import Server as ServerProxy
+    from xmlrpclib import Fault
+
+from ..configurator import Configurator
+
+# support unix domain socket connections
+# (we want to connect the same way as supervisorctl does for security and to automatically support multiple instances)
+def getServerProxy(serverurl=None, username=None, password=None):
+    # copied from https://github.com/Supervisor/supervisor/blob/b52f49cff287c4d821c2c54d7d1afcd397b699e5/supervisor/options.py#L1718
+    return ServerProxy(
+        # dumbass ServerProxy won't allow us to pass in a non-HTTP url,
+        # so we fake the url we pass into it and always use the transport's
+        # 'serverurl' to figure out what to attach to
+        "http://127.0.0.1",
+        transport=xmlrpc.SupervisorTransport(username, password, serverurl),
+    )
+
+
+def _reloadConfig(server, name):
+    result = server.supervisor.reloadConfig()
+    return any((name in changed) for changed in result[0])
+
+
+class SupervisorConfigurator(Configurator):
+    def run(self, task):
+        host = task.vars["HOST"]
+
+        # if homeDir is a relative path it will be relative to the baseDir of the host instance
+        # which might be different from the current directory if host is an external instance
+        confDir = os.path.abspath(os.path.join(host.context.baseDir, host["homeDir"]))
+        conf = host["conf"]
+
+        name = task.vars["SELF"]["name"]
+        confPath = os.path.join(confDir, "programs", name + ".conf")
+        if six.PY3:
+            parser = ConfigParser(inline_comment_prefixes=(";", "#"), strict=False)
+            parser.read_string(conf)
+        else:
+            parser = ConfigParser()
+            parser.readfp(six.StringIO(conf))
+        serverConfig = dict(parser.items("supervisorctl", vars=dict(here=confDir)))
+        serverConfig.pop("here", None)
+        server = getServerProxy(**serverConfig)
+
+        error = None
+        op = task.configSpec.operation
+        modified = False
+        try:
+            if op == "start":
+                server.supervisor.startProcess(name)
+                modified = True
+            elif op == "stop":
+                server.supervisor.stopProcess(name)
+                modified = True
+            elif op == "delete":
+                if os.path.exists(confPath):
+                    os.remove(confPath)
+                modified = _reloadConfig(server, name)
+            elif op == "configure":
+                program = task.vars["SELF"]["program"]
+                programDir = os.path.dirname(confPath)
+                task.logger.debug("writing %s", confPath)
+                if not os.path.isdir(programDir):
+                    os.makedirs(programDir)
+                with open(confPath, "w") as conff:
+                    conf = "[program:%s]\n" % name
+                    conf += "\n".join("%s= %s" % (k, v) for k, v in program.items())
+                    if "environment" not in program:
+                        conf += "\nenvironment= "
+                        conf += ",".join(
+                            '%s="%s"' % (k, v.replace("%", "%%"))
+                            for (k, v) in task.getEnvironment(True).items()
+                        )
+                    conff.write(conf)
+                modified = _reloadConfig(server, name)
+                server.supervisor.addProcessGroup(name)
+        except Fault as err:
+            if not (  # ok, 60 == ALREADY_STARTED
+                op == "start" and err.faultCode == 60
+            ) and not (  # ok, 70 == NOT_RUNNING
+                op == "stop" and err.faultCode == 70
+            ):
+                error = "supervisor error: " + str(err)
+
+        yield task.done(success=not error, modified=modified, result=error)
