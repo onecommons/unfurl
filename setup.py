e98e62d596901d66885b4275c0e4d6250a990cba
jozo <hi@jozo.io>
2021-06-11 13:49:00 +0200

    #62 - Fix extras requirements (#64)
    Move extras to setup.cfg

--- setup.py
+++ setup.py
@@ -4,15 +4,10 @@
 # Always prefer setuptools over distutils
 from setuptools import setup
 
-with open("full-requirements.txt") as f:
-    full_requirements = filter(
-        None, [line.partition("#")[0].strip() for line in f.read().splitlines()]
-    )
 
 setup(
     setup_requires=["pbr"],
     pbr=True,
     # XXX don't include .py files that haven't been committed to git
     include_package_data=True,
-    extras_require={"full": full_requirements},
 )
