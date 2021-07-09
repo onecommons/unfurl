a3fbc31ee34d0dbad996ab7cf6b90025f4443bbb
Adam Souzis <adam@souzis.com>
2021-07-09 04:15:04 -0700

    run smoketest tests in parallel

--- smoketest.sh
+++ smoketest.sh
@@ -1,1 +1,1 @@
-UNFURL_TEST_SKIP=docker+slow+k8s+helm+$UNFURL_TEST_SKIP tox -e ${1:-py39} -- -v --no-cov $2 $3 $4
+UNFURL_TEST_SKIP=docker+slow+k8s+helm+$UNFURL_TEST_SKIP tox -e ${1:-py39} -- -v --no-cov -n auto --dist loadfile $2 $3 $4 $5 $6 $7
