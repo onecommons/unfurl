8d876f9b2ccffb65d242b914e9ff60ab453c947f
Adam Souzis <adam@souzis.com>
2022-06-06 07:56:22 -0700

    Add reqcheck and noextraprops to UNFURL_VALIDATION_MODE

--- unfurl/tosca.py
+++ unfurl/tosca.py
@@ -192,11 +192,13 @@ class ToscaSpec:
 
     def _parse_template(self, path, inputs, toscaDef, resolver):
         # need to set a path for the import loader
-        ToscaTemplate.strict = True
         mode = os.getenv("UNFURL_VALIDATION_MODE")
+        ToscaTemplate.strict = False # XXX default to True
         additionalProperties = True # XXX default to False
         if mode is not None:
             additionalProperties = "additionalProperties" in mode
+            additionalProperties = "noextraprops" not in mode
+            ToscaTemplate.strict = "reqcheck" in mode
         EntityTemplate.additionalProperties = additionalProperties
         self.template = ToscaTemplate(
             path=path,
