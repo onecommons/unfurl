86c464dbc34808700741a02a0488a81f456162b9
Adam Souzis <adam@souzis.com>
2022-06-08 08:34:27 -0700

    Add a unit test for derived requirements

--- tests/test_syntax.py
+++ tests/test_syntax.py
@@ -26,7 +26,7 @@ class ManifestSyntaxTest(unittest.TestCase):
     def test_hasVersion(self):
         hasVersion = """
     apiVersion: unfurl/v1alpha1
-    kind: Manifest
+    kind: Ensemble
     spec: {}
     """
         assert YamlManifest(hasVersion)
@@ -34,7 +34,7 @@ class ManifestSyntaxTest(unittest.TestCase):
     def test_validateVersion(self):
         badVersion = """
     apiVersion: 2
-    kind: Manifest
+    kind: Ensemble
     spec: {}
     """
         with self.assertRaises(UnfurlError) as err:
@@ -50,10 +50,10 @@ class ManifestSyntaxTest(unittest.TestCase):
             "'apiVersion' is a required property", str(err.exception)
         )  # , <ValidationError: "'kind' is a required property">]''')
 
-    def test_template_inheritance(self):
+    def test_missing_includes(self):
         manifest = """
 apiVersion: unfurl/v1alpha1
-kind: Manifest
+kind: Ensemble
 templates:
   base:
     configurations:
@@ -67,106 +67,47 @@ spec:
             "missing includes: ['+/templates/production:']", str(err.exception)
         )
 
+    def test_template_inheritance(self):
+        manifest = """
+apiVersion: unfurl/v1alpha1
+kind: Ensemble
+spec:
+  service_template:
+    node_types:
+      Base:
+        derived_from: tosca:Root
+        requirements:
+        - host:
+            metadata:
+              base: meta
+              title: base host
+            relationship: tosca.relationships.DependsOn
+            description: A base compute instance
+            node: Base
 
-#   def test_badparams(self):
-#     # don't match spec definition
-#     manifest = '''
-# apiVersion: unfurl/v1alpha1
-# kind: Manifest
-#
-# configurators:
-#   step2:
-#     actions:
-#       install: foo
-# templates:
-#   base:
-#     configurations:
-#       - name: step1
-#         configurator:
-#           parameterSchema:
-#             - name: test
-#               type: string
-#               default: default
-#         parameters:
-#           test: base
-# resources:
-#   cloud3:
-#     spec:
-#       templates:
-#         - base
-#       # overrides and additions
-#       configurations:
-#         - name: base.step1
-#           parameters:
-#             # error: should be a string
-#             test: 0
-# '''
-#     with self.assertRaises(UnfurlValidationError) as err:
-#       m = YamlManifest(manifest)
-#     self.assertEqual(err and str(err.exception.errors[0][0]), "invalid value")
-#
-#   def test_unexpectedParam(self):
-#     #parameter missing from spec
-#     manifest = '''
-# apiVersion: unfurl/v1alpha1
-# kind: Manifest
-#
-# configurators:
-#   step2:
-#     actions:
-#       install: foo
-# templates:
-#   base:
-#     configurations:
-#       - name: step1
-#         configurator:
-#           parameterSchema:
-#             - name: test
-#               default: default
-#         parameters:
-#           test: base
-# resources:
-#   cloud3:
-#     spec:
-#       templates:
-#         - base
-#       # overrides and additions
-#       configurations:
-#         - name: base.step1
-#           parameters:
-#             doesntexist: True
-# '''
-#     with self.assertRaises(UnfurlValidationError) as err:
-#       YamlManifest(manifest)
-#     self.assertEqual(str(err.exception.errors[0][0]), "unexpected parameters")
-#
-#   def test_missingParam(self):
-#     #missing required parameter
-#     manifest = '''
-# apiVersion: unfurl/v1alpha1
-# kind: Manifest
-#
-# configurators:
-#   step2:
-#     actions:
-#       install: foo
-# templates:
-#   base:
-#     configurations:
-#       - name: step1
-#         configurator:
-#           parameterSchema:
-#             - name: test
-#               required: True
-# resources:
-#   cloud3:
-#     spec:
-#       templates:
-#         - base
-#       # overrides and additions
-#       configurations:
-#         - name: base.step1
-# '''
-#     with self.assertRaises(UnfurlValidationError) as err:
-#       YamlManifest(manifest)
-#     self.assertEqual(str(err.exception.errors[0][0]), "missing required parameter")
+      Derived:
+        derived_from: Base
+        requirements:
+        - host:
+            metadata:
+              title: derived host
+            description: A derived compute instance
+            node: Derived
+
+    topology_template:
+      node_templates:
+        the_app:
+          type: Derived
+          requirements:
+          - host:
+              node: the_app
+"""
+        manifest = YamlManifest(manifest)
+        req_def = manifest.tosca.nodeTemplates[
+          'the_app'].toscaEntityTemplate.type_definition.requirement_definitions['host']
+        assert req_def == {
+          'metadata': {'base': 'meta', 'title': 'derived host'},
+          'relationship': {'type': 'tosca.relationships.DependsOn'},
+          'description': 'A derived compute instance', 
+          'node': 'Derived'
+        }
