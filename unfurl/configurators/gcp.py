9cff983c56ffe8a19d4791d149dc32531c0958af
Adam Souzis <adam@souzis.com>
2022-07-07 09:32:45 -0700

    Add a configurator for validating and upgrading google cloud connections

---
+++ unfurl/configurators/gcp.py
@@ -0,0 +1,43 @@
+import os
+from ..configurator import Configurator
+from ..support import Status, Priority
+
+
+class CheckGooglCloudConnectionConfigurator(Configurator):
+    def can_dry_run(self, task):
+        # ok because this will also be called on the subtask
+        return True
+
+    def should_run(self, task):
+        try:
+            import google.auth
+        except ImportError:
+            task.logger.verbose("Skipping validation of Google Cloud connection: google.auth package not found")
+            return Priority.ignore
+
+        return Priority.critical
+
+    def run(self, task):
+        import google.auth
+
+        # check environment vars instead of connection attributes
+        # because we want to make sure they were set by the connection
+        status = Status.ok
+        if not os.getenv("GOOGLE_OAUTH_ACCESS_TOKEN"):
+            try:
+                credentials, project_id = google.auth.default()
+                if project_id and not os.getenv("CLOUDSDK_CORE_PROJECT"):
+                    os.environ["CLOUDSDK_CORE_PROJECT"] = project_id
+            except google.auth.exceptions.DefaultCredentialsError:
+                task.logger.error("unable to connect")
+                status = Status.error
+        elif not os.getenv("GOOGLE_APPLICATION_CREDENTIALS"):
+            # GOOGLE_OAUTH_ACCESS_TOKEN is set but GOOGLE_APPLICATION_CREDENTIALS isn't
+            # create an unfurl service account for the connection if template for that exists
+            if task.target.template.spec.get_template('unfurl_service_account'):
+                jobrequest, errors = task.update_instances(
+                    dict(name='unfurl_service_account', template='unfurl_service_account'))
+                job = yield jobrequest
+                status = job.status
+        yield task.done(True, status=status)
+
