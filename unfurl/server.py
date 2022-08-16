74f58e7506fc1c55ac37d7c6344e3c0c1743757a
tommcn <tommcn@mcnamer.ca>
2022-08-16 14:33:34 -0400

    Enhanced export and tests

--- unfurl/server.py
+++ unfurl/server.py
@@ -6,16 +6,18 @@ import uvicorn
 from flask import Flask, current_app, jsonify, request
 from flask_caching import Cache
 
-from unfurl.repo import Repo
 from unfurl.localenv import LocalEnv
+from unfurl.repo import Repo
+from unfurl.util import UnfurlError
 
 flask_config = {
     # Use in-memory caching, see https://flask-caching.readthedocs.io/en/latest/#built-in-cache-backends for more options
-    "CACHE_TYPE": "simple", 
+    "CACHE_TYPE": "simple",
 }
 app = Flask(__name__)
 app.config.from_mapping(flask_config)
 cache = Cache(app)
+
 
 @app.before_request
 def hook():
@@ -25,26 +27,41 @@ def hook():
     Authorization bearer token (Authorization=Bearer <secret>).
     """
     secret = current_app.config["UNFURL_SECRET"]
-    if secret is None: # No secret specified, no authentication required
+    if secret is None:  # No secret specified, no authentication required
         return
 
-    qs_secret = request.args.get("secret") # Get secret from query string
-    header_secret = request.headers.get('Authorization') # Get secret from Authorization header
+    qs_secret = request.args.get("secret")  # Get secret from query string
+    header_secret = request.headers.get(
+        "Authorization"
+    )  # Get secret from Authorization header
     if header_secret is not None:
         try:
             # Remove "Bearer " from header
-            header_secret = header_secret.split(' ')[1]
-        except IndexError: # Quick sanity check to make sure the header is formatted correctly
-            return jsonify({
-                "code": "BAD_REQUEST",
-                "message": "The Authorization header must be in the format 'Bearer <secret>'"
-            }), 400 # BAD_REQUEST
+            header_secret = header_secret.split(" ")[1]
+        except IndexError:  # Quick sanity check to make sure the header is formatted correctly
+            return (
+                jsonify(
+                    {
+                        "code": "BAD_REQUEST",
+                        "message": "The Authorization header must be in the format 'Bearer <secret>'",
+                    }
+                ),
+                400,
+            )  # BAD_REQUEST
 
-    if secret not in [qs_secret, header_secret]: # No valid secret found in headers or qs
-        return jsonify({
-            "code": "UNAUTHORIZED",
-            "message": "Please pass the secret as a query parameter or as an Authorization bearer token"
-        }), 401 # Unauthorized
+    if secret not in [
+        qs_secret,
+        header_secret,
+    ]:  # No valid secret found in headers or qs
+        return (
+            jsonify(
+                {
+                    "code": "UNAUTHORIZED",
+                    "message": "Please pass the secret as a query parameter or as an Authorization bearer token",
+                }
+            ),
+            401,
+        )  # Unauthorized
 
 
 @app.route("/health")
@@ -53,15 +70,22 @@ def health():
 
 
 @app.route("/export")
-@cache.cached(query_string=True) # Ensure that the request cached includes the query string (the response differs between different formats)
+@cache.cached(
+    query_string=True
+)  # Ensure that the request cached includes the query string (the response differs between different formats)
 def export():
     requested_format = request.args.get("format", "deployment")
     if requested_format not in ["blueprint", "environments", "deployment"]:
-        return jsonify({
-            "code": "BAD_REQUEST",
-            "message": "Query parameter 'format' must be one of 'blueprint', 'environments' or 'deployment'"
-        }), 400 # BAD_REQUEST
-    
+        return (
+            jsonify(
+                {
+                    "code": "BAD_REQUEST",
+                    "message": "Query parameter 'format' must be one of 'blueprint', 'environments' or 'deployment'",
+                }
+            ),
+            400,
+        )  # BAD_REQUEST
+
     # Default to exporting the ensemble provided to the server on startup
     path = current_app.config["UNFURL_ENSEMBLE_PATH"]
 
@@ -69,21 +93,30 @@ def export():
 
     # If asking for external repository
     if request.args.get("url") is not None:
-        git_url = unquote(request.args.get("url")) # Unescape the URL
-        repo = LocalEnv(current_app.config["UNFURL_ENSEMBLE_PATH"], can_be_empty=True).find_git_repo(git_url)
+        git_url = unquote(request.args.get("url"))  # Unescape the URL
+        repo = LocalEnv(
+            current_app.config["UNFURL_ENSEMBLE_PATH"], can_be_empty=True
+        ).find_git_repo(git_url)
 
         # Repo doesn't exists, clone it
         if not repo:
             from . import init
+
             init.clone(
-                git_url, 
-                clone_root + Repo.get_path_for_git_repo(git_url) + '/', 
+                git_url,
+                clone_root + Repo.get_path_for_git_repo(git_url) + "/",
                 empty=True,
                 var=(
-                    ['UNFURL_CLOUD_VARS_URL', request.args.get('cloud_vars_url')],
+                    [
+                        "UNFURL_CLOUD_VARS_URL",
+                        unquote(request.args.get("cloud_vars_url")),
+                    ],
                 ),
             )
-            repo = LocalEnv(clone_root + Repo.get_path_for_git_repo(git_url), can_be_empty=True).find_git_repo(git_url)
+            repo = LocalEnv(
+                clone_root + "/" + Repo.get_path_for_git_repo(git_url),
+                can_be_empty=True,
+            ).find_git_repo(git_url)
 
         deployment_path = request.args.get("deployment_path")
         if deployment_path:
@@ -91,13 +124,42 @@ def export():
         else:
             path = repo.working_dir
 
+    deployment_enviroment = request.args.get("environment")
+    if deployment_enviroment is None:
+        deployment_enviroment = current_app.config["UNFURL_OPTIONS"].get(
+            "use_environment"
+        )
 
     from . import to_json
-    local_env = LocalEnv(
-        path,
-        current_app.config["UNFURL_OPTIONS"].get("home"),
-        override_context=current_app.config["UNFURL_OPTIONS"].get("use_environment"),
-    )
+
+    try:
+        local_env = LocalEnv(
+            path,
+            current_app.config["UNFURL_OPTIONS"].get("home"),
+            override_context=deployment_enviroment,
+        )
+    except UnfurlError as e:
+        error_message = str(e)
+        # Sort of a hack to get the specific error since it only raises an "UnfurlError"
+        # Will break if the error message changes or if the Exception class changes
+        if "No environment named" in error_message:
+            return (
+                jsonify(
+                    {
+                        "code": "BAD_REQUEST",
+                        "message": f"No environment named '{deployment_enviroment}' found in the repository",
+                    }
+                ),
+                400,
+            )  # BAD_REQUEST
+        else:
+            return (
+                jsonify(
+                    {"code": "INTERNAL_ERROR", "message": "An internal error occurred"}
+                ),
+                500,
+            )
+
     exporter = getattr(to_json, "to_" + requested_format)
     json_summary = exporter(local_env)
 
