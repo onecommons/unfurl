d35066cae674c4364b4fa307eefc11a340717dff
Adam Souzis <adam@souzis.com>
2023-04-11 14:30:38 -0700

    logging: add option for logging invisible json and log task and job summary json

--- unfurl/logs.py
+++ unfurl/logs.py
@@ -1,4 +1,5 @@
 import collections.abc
+import json
 import logging
 import logging.config
 from enum import IntEnum
@@ -133,6 +134,11 @@ class ColorHandler(logging.StreamHandler):
         level = Levels[record.levelname]
         try:
             console = getConsole(file=self.stream)
+            data = getattr(record, "json", None)
+            if data and os.environ.get("CI", False):
+                # Running in a CI environment (eg GitLab CI)
+                console.out(json.dumps(data), end="\x1b[2K\r", highlight=False, style=None)
+
             console.print(
                 f"[{self.RICH_STYLE_LEVEL[level]}] {level.name.center(8)}[/]", end=""
             )
