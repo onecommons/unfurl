1d24501ba477bfc4d1f44461e83f53e82fa1fde2
Robert Detjens <github@detjens.dev>
2024-01-16 13:34:57 -0800

    Add additional server image with nginx cache included (#275)
    * add addl server image with nginx cache included
    
    * remove minimum request count req for nginx cache image
    
    * use server image as base for server-cached image
    
    * use short sha for cache image FROM instead of raw branch
    
    * use short sha for cache image FROM instead of raw branch
    
    * docker metadata sha is only seven chars

---
+++ docker/aux/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# start nginx
+nginx -g "daemon off;" &
+
+# start unfurl-server as unfurl user
+gosu unfurl:unfurl gunicorn -b=0.0.0.0:5001 -w=${NUM_WORKERS} unfurl.server:app &
+
+# wait for any process to exit
+wait -n
+
+# exit with status of process that exited first
+exit $?
