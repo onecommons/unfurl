78a0940fcc50ceef37f5687cf9ca5e1e7792a301
Robert Detjens <github@detjens.dev>
2024-01-29 18:50:51 -0800

    pass any entrypoint args to unfurl-server gunicorn

--- docker/aux/entrypoint.sh
+++ docker/aux/entrypoint.sh
@@ -4,7 +4,7 @@
 nginx -g "daemon off;" &
 
 # start unfurl-server as unfurl user
-gosu unfurl:unfurl gunicorn -b=0.0.0.0:5001 -w=${NUM_WORKERS} unfurl.server:app &
+gosu unfurl:unfurl gunicorn -b=0.0.0.0:5001 -w=${NUM_WORKERS} unfurl.server:app $* &
 
 # wait for any process to exit
 wait -n
