f5732f8debae560e98ccf5152ea31b0cba67debe
Adam Souzis <adam@souzis.com>
2024-03-15 13:43:55 -0700

    dsl: smarter overwrite policy

--- tosca-package/tosca/_tosca.py
+++ tosca-package/tosca/_tosca.py
@@ -3182,7 +3182,7 @@ class WritePolicy(Enum):
 
     def deny_message(self, unchanged=False) -> str:
         if unchanged:
-            return 'overwrite policy is "auto" but the contents have not changed'
+            return f'overwrite policy is "{self.name}" but the contents have not changed'
         if self == WritePolicy.auto:
             return 'overwrite policy is "auto" and the file was last modified by another process'
         if self == WritePolicy.never:
@@ -3205,6 +3205,10 @@ class WritePolicy(Enum):
         self, input_path: str, output_path: str, new_src: Optional[str] = None
     ) -> Tuple[bool, bool]:
         if self == WritePolicy.always:
+            if new_src and os.path.exists(output_path):
+                with open(output_path) as out:
+                    contents = out.read()
+                return True, self.has_contents_unchanged(new_src, contents)
             return True, False
         if self == WritePolicy.never:
             return not os.path.exists(output_path), False
@@ -3232,8 +3236,8 @@ class WritePolicy(Enum):
     def has_contents_unchanged(self, new_src: Optional[str], old_src: str) -> bool:
         if new_src is None:
             return False
-        new_lines = [l.strip() for l in new_src.splitlines() if not l.startswith("#")]
-        old_lines = [l.strip() for l in old_src.splitlines() if not l.startswith("#")]
+        new_lines = [l.strip() for l in new_src.splitlines() if l.strip() and not l.startswith("#")]
+        old_lines = [l.strip() for l in old_src.splitlines() if l.strip() and not l.startswith("#")]
         if len(new_lines) == len(old_lines):
             return new_lines == old_lines
         return False
