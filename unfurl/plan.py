bf162c9c88ea62d4615c284423ab55227f5681b7
Adam Souzis <adam@souzis.com>
2024-07-14 19:43:43 -0700

    plan: exclude aggregate only in undeploy dependency check

--- unfurl/plan.py
+++ unfurl/plan.py
@@ -481,6 +481,8 @@ class Plan:
             return None
         if resource.shadow or resource.template.abstract:
             skip = "read-only instance"
+        elif resource.template.aggregate_only():
+            skip = "virtual instance"
         elif not resource.created and not self.jobOptions.destroyunmanaged:
             skip = "instance wasn't created by this ensemble"
         elif isinstance(resource.created, str) and not ChangeRecord.is_change_id(
@@ -500,8 +502,8 @@ class Plan:
         if not reason:
             skip = "didn't meet removal criteria"
         if skip:
-            logger.verbose("skip instance '%s' for removal: '%s'", resource.name, skip)
-            if not self.jobOptions.force and resource.operational and resource.required:
+            logger.verbose("skip instance '%s' for removal: '%s' (%s)", resource.name, skip, resource.status)
+            if not self.jobOptions.force and resource.operational and resource.required and not resource.template.aggregate_only():
                 return "cancelled"
             else:
                 return None
