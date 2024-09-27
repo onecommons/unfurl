7b6544e0bd94a19bf8a18ef8889d2dad03bcbfff
Adam Souzis <adam@souzis.com>
2024-09-27 11:16:41 -0700

    dsl: yaml to python: support inline relationship templates on node templates

--- docs/examples/configurators-8.py
+++ docs/examples/configurators-8.py
@@ -1,5 +1,8 @@
-from unfurl.configurators.templates.dns import unfurl_nodes_DNSZone
+import unfurl
 import tosca
+from tosca import Eval
+
+from unfurl.configurators.templates.dns import unfurl_nodes_DNSZone, unfurl_relationships_DNSRecords
 
 example_com_zone = unfurl_nodes_DNSZone(
     "example_com_zone",
@@ -11,4 +14,16 @@ test_app = tosca.nodes.WebServer(
     "test_app",
     host=[tosca.find_node("compute")],
 )
-test_app.dns = example_com_zone
+test_app.dns = unfurl_relationships_DNSRecords(
+    "",
+    records=Eval(
+        {
+            "www": {
+                "type": "A",
+                "value": {
+                    "eval": ".source::.requirements::[.name=host]::.target::public_address"
+                },
+            }
+        }
+    ),
+)[example_com_zone]
