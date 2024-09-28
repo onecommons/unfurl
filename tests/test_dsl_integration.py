29d73351377188160c988cc5ec437d924e47ade4
Adam Souzis <adam@souzis.com>
2024-09-28 16:10:29 -0700

    fix(dsl): have to_XXX_label functions evaluate arguments as needed.

--- tests/test_dsl_integration.py
+++ tests/test_dsl_integration.py
@@ -179,6 +179,8 @@ def test_expressions():
         path1: str = expr.get_dir(None, "src")
         default_expr: str = expr.fallback(None, "foo")
         or_expr: str = expr.or_expr(default_expr, "ignored")
+        label: str = functions.to_dns_label(expr.get_input("missing", "fo!o"))
+
     class test(tosca.Namespace):
         service = Service()
         test_node = Test()
@@ -192,7 +194,7 @@ def test_expressions():
     assert expr.get_nodes_of_type(Service) == [topology.service]
     assert expr.get_input("MISSING", "default") == "default"
     with pytest.raises(UnfurlError):
-        assert expr.get_input("MISSING")
+        input: str = expr.get_input("MISSING")
     assert expr.get_dir(topology.service, "src").get() == os.path.dirname(__file__)
     # XXX assert topology.test_node.path1 == os.path.dirname(__file__)
     assert expr.abspath(topology.service, "test_dsl_integration.py", "src").get() == __file__
@@ -202,6 +204,7 @@ def test_expressions():
     assert expr.template(topology.test_node, contents="{%if 1 %}{{SELF.url}}{%endif%}") == "#::test.test_node"
     assert topology.test_node.default_expr == "foo"
     assert topology.test_node.or_expr == "foo"
+    assert topology.test_node.label == "fo--o"
     # XXX test:
     # "if_expr", and_expr
     # "lookup",
