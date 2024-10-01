b381d4adc51be8cb3c37d7646ff30ab1e79c0d7e
Adam Souzis <adam@souzis.com>
2024-09-30 17:16:52 -0700

    gui: add methods for reloading config files and clean up secret managers.

--- unfurl/server/gui_variables/local_secrets.py
+++ unfurl/server/gui_variables/local_secrets.py
@@ -1,5 +1,5 @@
 import os
-from typing import Iterator, List, Literal, Union
+from typing import Any, Dict, Iterator, List, Literal, Optional, Union, cast
 
 from ...logs import is_sensitive, getLogger
 
@@ -14,68 +14,84 @@ UNFURL_SERVE_PATH = os.getenv("UNFURL_SERVE_PATH", "")
 logger = getLogger("unfurl.gui")
 
 
-def set_variables(localenv, env_vars: List[EnvVar]) -> LocalEnv:
+def _get_env_vars(envs: Dict[str, dict], env_name: str) -> Dict[str, Any]:
+    env = envs.get(env_name)
+    if env:
+        return env.get("variables", {})
+    return {}
+
+
+def _set_env_var(environments: Dict[str, dict], env_name: str, key: str, val: Any) -> Dict[str, Any]:
+    env = environments.setdefault(env_name, CommentedMap())
+    variables = env.setdefault("variables", CommentedMap())
+    variables[key] = val
+    return variables
+
+
+def set_variables(localenv: LocalEnv, env_vars: List[EnvVar]) -> None:
     # reload immediately in case of user edits
-    localenv = LocalEnv(UNFURL_SERVE_PATH, overrides={"ENVIRONMENT": "*"})
     project = localenv.project or localenv.homeProject
     assert project
-    secret_config_key, secret_config = project.localConfig.find_secret_include()
-    secret_environments = (
-        secret_config.setdefault("environments", CommentedMap())
-        if secret_config
-        else None
-    )
-    config: dict = project.localConfig.config.config
+    project.reload()
+
+    config = cast(Dict[str, Dict], project.localConfig.config.config)
     assert config
-    env = config.setdefault("environments", CommentedMap())
+    envs = config.setdefault("environments", CommentedMap())
+
+    secret_config_key, secret_config = project.localConfig.find_secret_include()
+    if secret_config is not None:
+        secret_environments = cast(
+            Optional[Dict[str, Dict]],
+            secret_config.setdefault("environments", CommentedMap()),
+        )
+    else:
+        secret_environments = None
+
     modified_secrets = False
     modified_config = False
     for envvar in env_vars:
         environment_scope = envvar["environment_scope"]
         env_name = "defaults" if environment_scope == "*" else environment_scope
+        config_env_vars = _get_env_vars(envs, env_name)
+        if secret_environments is not None:
+            secret_env_vars = _get_env_vars(secret_environments, env_name)
+        else:
+            secret_env_vars = {}
         key = envvar["key"]
         value = envvar.get("secret_value", envvar.get("value"))
         if envvar["variable_type"] == "file":
             value = {"eval": dict(tempfile=value)}
         if envvar.get("_destroy"):
-            if env_name in env and key in env[env_name]:
+            if key in config_env_vars:
                 modified_config = True
-                del env[env_name][key]
-            secret_env = (
-                secret_environments.get(env_name) if secret_environments else None
-            )
-            if secret_env and key in secret_env.get("variables", {}):
+                del config_env_vars[key]
+            if key in secret_env_vars:
                 modified_secrets = True
-                del secret_env["variables"][key]
+                del secret_env_vars[key]
         else:
             if envvar["masked"]:
-                env.pop(key, None)  # in case this flag changed
-                if secret_environments is not None:
-                    secret_env = secret_environments.setdefault(
-                        env_name, CommentedMap()
-                    )
+                secret_val = {"eval": dict(sensitive=value)}  # mark sensitive
+                if secret_environments:
+                    _set_env_var(secret_environments, env_name, key, secret_val)
                     modified_secrets = True
-                    secret_env.setdefault("variables", {})[key] = {
-                        "eval": dict(sensitive=value)
-                    }  # mark sensitive
+                    if key in config_env_vars:
+                        modified_config = True
+                        del config_env_vars[key]
+                else:
+                    _set_env_var(envs, env_name, key, secret_val)
+                    modified_config = True
             else:
-                # in case this flag changed
-                secret_env = (
-                    secret_environments.get(env_name) if secret_environments else None
-                )
-                if secret_env and key in secret_env.get("variables", {}):
+                if key in secret_env_vars:  # in case masked flag changed
                     modified_secrets = True
-                    del secret_env["variables"][key]
+                    del secret_env_vars[key]
+                _set_env_var(envs, env_name, key, value)
                 modified_config = True
-                env.setdefault(env_name, CommentedMap())[key] = value
     if modified_secrets:
         project.localConfig.config.save_include(secret_config_key)
     if modified_config:
         project.localConfig.config.save()
     if modified_secrets or modified_config:
-        # reload
-        localenv = LocalEnv(UNFURL_SERVE_PATH, overrides={"ENVIRONMENT": "*"})
-    return localenv
+        project.reload()
 
 
 def yield_variables(localenv) -> Iterator[EnvVar]:
