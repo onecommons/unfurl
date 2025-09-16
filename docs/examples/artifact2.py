b82b770da7a2dd497686921b604af4996599242b
Adam Souzis <adam@souzis.com>
2025-09-16 00:59:28 -0700

    docs: update artifact chapter

--- docs/examples/artifact2.py
+++ docs/examples/artifact2.py
@@ -1,22 +1,30 @@
 import unfurl
 import tosca
 from tosca import ToscaOutputs, Attribute, Eval
+
+
 class MyArtifact(unfurl.artifacts.ShellExecutable):
     file: str = "myscript.sh"
-    contrived_key: str
-    outputsTemplate = Eval("{{ stdout | from_json | subelements(SELF.contrived_key)}}")
+
+    # evaluates the script's output
+    outputsTemplate = Eval("{{ stdout | from_json }}")
 
     class Outputs(ToscaOutputs):
-        a_output: str = Attribute()
+        output1: str = Attribute()
+        output2: int = Attribute()
 
+    # no implementation is needed for execute() because:
+    # - execute() arguments are passed as command line arguments by default
+    # - MyArtifact.Outputs is constructed from the json map returned by outputsTemplate
     def execute(self, arg1: str, arg2: int) -> Outputs:
         return MyArtifact.Outputs()
 
+
 class MyNode(tosca.nodes.Root):
-    prop1: str
-    prop2: int
+    prop: int
 
     def configure(self) -> MyArtifact.Outputs:
-        return MyArtifact(contrived_key=self.prop1).execute("hello", arg2=self.prop2) 
+        return MyArtifact(input="y").execute("hello", self.prop)
 
-my_node = MyNode(prop1="foo", prop2=1)
+
+my_node = MyNode(prop=1)
